89926d6f8f1d6395b1793f86848e1806a5ae805e
AwallenceCode <awallencecode@gmail.com>
2025-10-06 23:32:34 +0300

    Initial commit

---
+++ StaticFields/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Enemy
+{
+    private readonly string _name;
+    private byte _health;
+    private static uint _totalEnemies;
+
+    public Enemy(string name, byte health, uint totalEnemies)
+    {
+        if (string.IsNullOrEmpty(name)) throw new ArgumentException("Description cannot be empty!");
+    }
+}
+
+class MyClass
+{
+    static void Main()
+    {}
+}
